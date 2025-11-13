db6522312f9c48222e580371d1b03771d88c59c7
Richard Hoskin <10529434@cityplym.ac.uk>
2025-11-13 16:23:12 +0000

    Menu system added 13/11/2025 16:30

--- src/Application.java
+++ src/Application.java
@@ -5,28 +5,61 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         NodeService nodeService = new NodeService();
-        //This is added for testing to ensure the added resources are counted
-        nodeService.addResource(ResourceType.NODE, "Node 1    ","Computer      ", "A computer is what you use to carry out tasks" );
-        nodeService.addResource(ResourceType.NODE, "Node 2    ","Programming   ","Programming is used to main things happen");
-        nodeService.addResource(ResourceType.NODE, "Node 3    ","Databases     ",  "Databases store the data for use at a later time");
-
-        System.out.println();
-        nodeService.totalItems();
-
-        //added 13/11/2025 12:35 Used for Searching
-        //Java for reading in a console in Intellij - https://www.w3schools.com/java/java_user_input.asp accessed 13/11/2025
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\nPLEASE ENTER SOMETHING TO SEARCH FOR: ");
-        String term = scanner.nextLine();
 
-        var results = nodeService.searchResources(term);
+        // Preload some resources for testing
+        nodeService.addResource(ResourceType.NODE, "Node 1", "Computer", "A computer is what you use to carry out tasks");
+        nodeService.addResource(ResourceType.NODE, "Node 2", "Programming", "Programming is used to make things happen");
+        nodeService.addResource(ResourceType.NODE, "Node 3", "Databases", "Databases store the data for use at a later time");
 
-        if (results.isEmpty()) {
-            System.out.println("SORRY NO MATCHES ARE FOUND PLEASE RETRY ANOTHER SEARCH");
-        } else {
-            System.out.println("\nSEARCH RESULTS:");
-            results.forEach(System.out::println);
-            //end of Search
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nRESEARCHER MENU");
+            System.out.println("SELECT 1 Add a new Node");
+            System.out.println("SELECT 2 Search for resources");
+            System.out.println("SELECT 3 Show total number of resources");
+            System.out.println("SELECT 4 Edit node");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.print("Enter Node ID in format of your initals followed by number: ");
+                    String nodeid = scanner.nextLine();
+
+                    System.out.print("Enter title for the Node: ");
+                    String title = scanner.nextLine();
+
+                    System.out.print("Enter the content for the Node: ");
+                    String content = scanner.nextLine();
+
+                    nodeService.addResource(ResourceType.NODE, nodeid, title, content);
+                    System.out.println("Node has been added");
+                    break;
+
+                case "2":
+                    System.out.print("Enter search term: ");
+                    String term = scanner.nextLine();
+                    var results = nodeService.searchResources(term);
+
+                    if (results.isEmpty()) {
+                        System.out.println("Nothing stored please carry out a new search.");
+                    } else {
+                        System.out.println("\nSEARCH RESULTS:");
+                        results.forEach(System.out::println);
+                    }
+                    break;
+
+                case "3":
+                    nodeService.totalItems();
+                    break;
+
+                default:
+                    System.out.println("Invalid option, please select 1-4.");
+            }
         }
+
+        scanner.close();
     }
 }
