1ba4f9495aa3b7a3a49395e520ddce11d3ac2e1d
Richard Hoskin <10529434@cityplym.ac.uk>
2025-11-13 15:50:49 +0000

    Updates made 13/11/2025 15:50

--- src/models/Researcher.java
+++ src/models/Researcher.java
@@ -2,30 +2,40 @@ package models;
 
 public class Researcher {
     //private Integer id;
-    private String firstName;
-    private String lastName;
+    private String fullName;
+    private String email;
+    private String password;
 
     //public Integer getId() {
-        //return id;
+    //return id;
     //}
 
     //public void setId(Integer id) {
-        //this.id = id;
+    //this.id = id;
     //}
 
-    public String getFirstName() {
-        return firstName;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getEmail() {
+        return email;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
     }
 }
